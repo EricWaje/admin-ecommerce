608aec1174cc6c77dd5a2b6cfbe0dfb05de91637
Eric Wajnrajch <ericwaje03@gmail.com>
2022-01-08 11:31:52 -0300

    sacando el login

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Footer from './components/Footer';
 import Home from './components/Home/Home';
 import ProdForm from './components/ProdForm';
 import EditProd from './components/EditProd';
-import Login from './components/Login';
 
 function App() {
     return (
@@ -15,8 +14,7 @@ function App() {
                     <Navbar />
                 </div>
                 <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/home" element={<Home />} />
+                    <Route path="/" element={<Home />} />
                     <Route path="/create" element={<ProdForm />} />
                     <Route path="/edit/:id" element={<EditProd />} />
                 </Routes>
